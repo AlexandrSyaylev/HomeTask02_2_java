6bfa89f81a9c31b649c9767ba0885216286de4fe
Alexander Syayalev <alexxs@inbox.ru>
2022-01-20 14:13:16 +0300

    change calculating bonus up for 1000

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 //Lection 2 Hometask 2.2 about bonus for each full 100 Rub adding
 public class Main {
     public static void main(String[] args) {
-        int balance = 99 ;
-        int addingFunds = 100;
+        int balance = 100 ;
+        int addingFunds = 1110;
         int bonus  =0;
 
         System.out.println("Ваш старый баланс " + balance);
 
-        if (addingFunds >= 100) {
+        if (addingFunds >= 1000) {
             bonus = addingFunds / 100;
         }
         //Иначе бонус не меняется, а он объявлен 0
